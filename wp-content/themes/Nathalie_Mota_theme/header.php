f5b5f27fcd0f298617f0f9596fdd56c690105c32
dahut03 <pamultirix@hotmail.fr>
2024-07-29 01:44:49 +0200

    v2

---
+++ wp-content/themes/Nathalie_Mota_theme/header.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header class="header">
+        <div class="logo">MonLogo</div>
+        <nav>
+            <ul class="menu">
+                <li><a href="#home">Accueil</a></li>
+                <li><a href="#about">À propos</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
