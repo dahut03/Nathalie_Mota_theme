21809cbc8863965af781eb3602b900eb5872bc86
dahut03 <160189008+dahut03@users.noreply.github.com>
2024-07-31 00:17:28 +0200

    Add files via upload

--- wp-content/themes/Nathalie_Mota_theme/header.php
+++ wp-content/themes/Nathalie_Mota_theme/header.php
@@ -1,22 +1,40 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * @package OceanWP WordPress theme
+ */
 
+?>
 <!DOCTYPE html>
-<html lang="fr">
+<html class="<?php echo esc_attr( oceanwp_html_classes() ); ?>" <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<?php wp_head(); ?>
 </head>
-<body>
-    <header class="header">
-        <div class="logo">MonLogo</div>
-        <nav>
-            <ul class="menu">
-                <li><a href="#home">Accueil</a></li>
-                <li><a href="#about">À propos</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
-</body>
-</html>
+
+<body <?php body_class(); ?> <?php oceanwp_schema_markup( 'html' ); ?>>
+
+	<?php wp_body_open(); ?>
+
+	<?php do_action( 'ocean_before_outer_wrap' ); ?>
+
+	<div id="outer-wrap" class="site clr">
+
+		<a class="skip-link screen-reader-text" href="#main"><?php echo esc_html( oceanwp_theme_strings( 'owp-string-header-skip-link', false ) ); ?></a>
+
+		<?php do_action( 'ocean_before_wrap' ); ?>
+
+		<div id="wrap" class="clr">
+
+			<?php do_action( 'ocean_top_bar' ); ?>
+
+			<?php do_action( 'ocean_header' ); ?>
+
+			<?php do_action( 'ocean_before_main' ); ?>
+
+			<main id="main" class="site-main clr"<?php oceanwp_schema_markup( 'main' ); ?> role="main">
+
+				<?php do_action( 'ocean_page_header' ); ?>
